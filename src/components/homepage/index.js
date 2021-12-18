24dd0cf64812a272a8a4c60bb1b35054614b78fb
Akinolae <makindeakinola22@gmail.com>
2021-12-18 21:33:27 +0100

    added changes to landing page

--- src/components/homepage/index.js
+++ src/components/homepage/index.js
@@ -1,36 +1,90 @@
 import React from 'react'
-import { Col, Container, Row } from 'reactstrap'
-import styled from 'styled-components'
+import { Col, Row } from 'reactstrap'
 import { Link } from 'react-router-dom'
-import QRCode from 'react-qr-code'
+import { Button, Text } from '../signin/signin'
 
-const Nav = styled.div`
-    height: 50px;
-`
 const HomePage = () => {
     return (
-        <div style={{ height: 100 }}>
-            <Container>
-                <Col>
-                    <Row>
-                        <Nav className="d-flex justify-content-between align-items-center">
-                            <Col xl={6}></Col>
-                            <Col
-                                xl={6}
-                                className="d-flex justify-content-between"
+        <Col
+            className="pt-3"
+            style={{
+                height: '100%',
+                position: 'fixed',
+                top: 0,
+                left: 0,
+                bottom: 0,
+                right: 0,
+                backgroundColor: '#065340',
+            }}
+        >
+            <Col>
+                <Row className="d-flex justify-content-between align-items-center">
+                    <Col lg={6} sm={12}>
+                        <p>Afrobank</p>
+                    </Col>
+                    <Col className="d-flex justify-content-end " lg={6}>
+                        <Link to="/signin">
+                            <Button
+                                className="rounded-pill"
+                                style={{
+                                    backgroundColor: 'white',
+                                    color: '#065340',
+                                }}
                             >
-                                <Link to="/signin">sign in</Link>
-                                <Link to="/register">register</Link>
-                            </Col>
-                        </Nav>
-                    </Row>
-                </Col>
-                <Col>
-                    <QRCode value="Akinola" />
-                </Col>
-            </Container>
+                                sign in
+                            </Button>
+                        </Link>
+                    </Col>
+                </Row>
+            </Col>
+            <Col
+                style={{ height: '100%' }}
+                className="d-flex justify-content-between align-items-center"
+            >
+                <Row>
+                    <Col lg={4} sm={12}>
+                        <div>
+                            <Text
+                                style={{
+                                    color: 'white',
+                                }}
+                            >
+                                Afrobank is your life long partner
+                            </Text>
+                            <Text
+                                style={{
+                                    color: 'white',
+                                    fontSize: '24px',
+                                    fontWeight: '500',
+                                    opacity: 0.6,
+                                }}
+                            >
+                                401k taxpayer retirement capitalization
+                                consulting prices interest rate established
+                                gains exchange traded funds financial health.
+                                Credit shares fluctuate.
+                            </Text>
+                            <Link className="mt-4" to="/register">
+                                <Button
+                                    className="rounded-pill "
+                                    style={{
+                                        backgroundColor: 'white',
+                                        color: '#065340',
+                                        width: '60%',
+                                        height: '55px',
+                                        fontSize: '20px',
+                                    }}
+                                >
+                                    Let's get you started
+                                </Button>
+                            </Link>
+                        </div>
+                    </Col>
+                </Row>
+            </Col>
+
             {/* <TransferModal /> */}
-        </div>
+        </Col>
     )
 }
 
