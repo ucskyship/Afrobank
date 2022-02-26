578984ac3fe5b354b534f51f4636ee8281f5bf3a
Akinolae <makindeakinola22@gmail.com>
2022-02-26 17:28:13 +0100

    clean up

--- src/components/homepage/index.js
+++ src/components/homepage/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Col, Container, Row } from 'reactstrap'
+import { Col, Row } from 'reactstrap'
 import { Link } from 'react-router-dom'
 import { Button, Text } from '../signin/signin'
 
